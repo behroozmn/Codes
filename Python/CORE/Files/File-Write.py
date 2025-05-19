3be7c6b66e8f02b7408ada794dc6c99eea72c5e1
بهروز محمدی نسب <b.mohamadi@storex.local>
2025-05-19 14:26:23 +0330

    14040229-14:26

--- Python/CORE/Files/File-Write.py
+++
@@ -1,8 +0,0 @@
-# mode:
-# a: append
-# w: read
-# r: write
-
-
-with open("/tmp/salam.txt", encoding='UTF-8', mode="w") as bFile:
-    bFile.write("STRIIIIIIIIIIIIIIIIIIIIIIIIIIING\n")
--- Python/CORE/LeadingZero.py
+++
@@ -1,3 +0,0 @@
-number = 1
-number = f"{number:03d}"
-print(number)
